95979acb0573401e4d9ea5d9384ce6bc6e7a529d
lee jungeun <jungeun623@gmail.com>
2024-07-30 16:00:51 +0900

    이미지 첨부, 헤더부분 반응형

---
+++ js/header.js
@@ -0,0 +1,15 @@
+window.addEventListener("load",function(){
+    // 헤더를 스크롤할때 헤더변경기능
+    const header = this.document.querySelector(".header");
+    let scy = 0;
+    
+    // console.log(scy);
+    this.window.addEventListener("scroll" , function(){
+        scy = this.window.document.documentElement.scrollTop;
+        if(scy > 0){
+            header.classList.add("active");
+        }else{
+            header.classList.remove("active");
+        }
+    })
+});
